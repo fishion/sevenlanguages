469bc6d00580377eb58e248d9fb57b99538daac6
Alex <alex@monney.me>
2013-11-03 22:43:26 +0000

    barber shop working

--- clojure/day3/hw_barber.clj
+++ clojure/day3/hw_barber.clj
@@ -20,47 +20,57 @@
 (defmethod say :Barber [who, thing]
     (println (str "BARBER SAYS : " thing)))
 
+; set up some variables
+(def waitingchairs 3)
+(def mininterval 10)
+(def maxinterval 30)
+(def intervaldelta (- maxinterval mininterval))
+(def haircuttime 20)
+(def openinghours 10000)
+
 ; set up a reference and a barber agent for async working
 (def chairs (ref {:waiting 0, :beingcut 0}))
 (def barb (agent 0))
 
 
 (defn cut-hair [customers_done, chairs]
-    (say barber "checking waiting room")
     (if (and (= (chairs :beingcut) 0)
             (> (chairs :waiting) 0) )
         (do
             (say barber "Next please")
             (dosync(
-                (alter chairs assoc :beingcut 1)
                 (alter chairs assoc :waiting (- (chairs :waiting) 1))
+                (alter chairs assoc :beingcut 1)
             ))
             (say barber (str "Cutting Hair of customer " (+ customers_done 1)))
-            (Thread/sleep 1000)
+            (Thread/sleep haircuttime)
             (say barber (str "Done cutting Hair of customer " (+ customers_done 1)))
-            (say barber (str "And check out my chairs " (@chairs :waiting)) )
             (dosync(
-                alter chairs assoc :beingcut (- (chairs :beingcut) 1)
+                alter chairs assoc :beingcut 0
             ))
-            (+ 1 customers_done))
+            (cut-hair (+ 1 customers_done) chairs))
         (do 
             (say barber "nobody there")
             customers_done))
 )
 
-(defn open-shop [barb, chairs]
-    ; maybe use a sequence of random numbers to iterate over until shop closes
-    (say customer "Do I need a haircut?")
-    (Thread/sleep 1000)
-    (say customer "OK, I'm going in")
+(defn open-shop [barb, chairs, timer]
+    ; alternatively use a sequence of random numbers to iterate over until shop closes
+    (def n (+ mininterval (rand intervaldelta)))
+    (Thread/sleep n)
+    (say customer (str "OK, I waited " (/ n 1000) " seconds and now I'm going in"))
     (dosync (
-        if (< (chairs :waiting) 2)
+        if (< (chairs :waiting) waitingchairs)
             (alter chairs assoc :waiting (+ (chairs :waiting) 1) )
             (say customer "Meh. Too full.")
     ))
-    (send barb cut-hair chairs)
-    (send barb cut-hair chairs)
+    (if (= (chairs :beingcut) 0)
+        (send barb cut-hair chairs)
+    )
+    (if (< ( + n timer ) openinghours)
+        ( open-shop barb chairs (+ n timer) ) )
+    str "Shop closed"
 )
 
 ; open shop
-(open-shop barb chairs)
+(open-shop barb chairs 0)
